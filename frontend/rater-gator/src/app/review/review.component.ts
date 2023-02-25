1fa75f28a5e02339bd9d7f21a18016f927368413
Shane Ferrell <shaneferrellwv@gmail.com>
2023-02-25 13:19:53 -0500

    added component to create new reviews

--- frontend/rater-gator/src/app/review/review.component.ts
+++ frontend/rater-gator/src/app/review/review.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ReviewService, Review } from './review.service';
+import { CreateReviewComponent } from '../create-review/create-review.component';
 
 @Component({
   selector: 'app-review',
@@ -7,6 +8,7 @@ import { ReviewService, Review } from './review.service';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent {
+  // list of reviews
   reviews: Review[] = [];
 
   constructor(private service: ReviewService) {}
--- frontend/rater-gator/src/app/review/review.service.ts
+++ frontend/rater-gator/src/app/review/review.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { CreateReviewComponent } from '../create-review/create-review.component';
 
+// review object, has 4 properties of user-entered info
 export interface Review {
   location: string;
   rating: number;
@@ -11,21 +13,30 @@ export interface Review {
   providedIn: 'root'
 })
 export class ReviewService {
+  // service list for reviews
+  // initalized with one fake review
+  static _reviews: Review[] = [{
+    location: "Marston",
+    rating: 5,
+    description: "comfy",
+    author: "Shane",
+  // },
+  // {
+    // location: "Carleton",
+    // rating: 2,
+    // description: "uncomfy",
+    // author: "Devala",
+  }];
 
   constructor() { }
 
+  // reviews list accessor
   static getReviews(): Review[] {
-    return [{
-      location: "Marston",
-      rating: 5,
-      description: "comfy",
-      author: "Shane",
-    },
-    {
-      location: "Carleton",
-      rating:25,
-      description: "uncomfy",
-      author: "Devala",
-    }]
+    return this._reviews;
+  }
+
+  // new reviews accessor
+  static getNewReviews() {
+    this._reviews.push(CreateReviewComponent.newReviews[0]);
   }
 }
