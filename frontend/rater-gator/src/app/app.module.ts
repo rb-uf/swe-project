632774d3f5b158aab79ac6d6ebfedfe05d88a1b7
devala-g <n.griffith@ufl.edu>
2023-04-04 15:32:02 -0400

    generated angular application

--- frontend/rater-gator/src/app/app.module.ts
+++ frontend/rater-gator/src/app/app.module.ts
@@ -1,43 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReviewComponent } from './review/review.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SubmitButtonComponent } from './review/create-review/submit-button/submit-button.component';
-import { CreateReviewComponent } from './review/create-review/create-review.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { SubjectComponent } from './review/subject/subject.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ReviewComponent,
-    SubmitButtonComponent,
-    CreateReviewComponent,
-    SubjectComponent,
-    ToolbarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatSelectModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
